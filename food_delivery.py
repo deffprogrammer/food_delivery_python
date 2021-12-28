0f3fe76b31e2c1a04e0d68b282f83beb20a93207
Deff_ <96669422+deffprogrammer@users.noreply.github.com>
2021-12-28 07:32:23 +0700

    add order method feature

--- food_delivery.py
+++ food_delivery.py
@@ -33,10 +33,32 @@ while my_bool:
     else:
         my_bool = False
 
+order_method = input("Order online/offline : ")
+
+if order_method.lower() == "online":
+    print("Services")
+    print("1. JNE\t: 100")
+    print("2. JNT\t: 200")
+    expedition = input("Choose using number : ")
+    if expedition == "1":
+        total += 100
+        expedition = "JNE"
+    elif expedition == "2":
+        total += 200
+        expedition = "JNT"
+elif order_method.lower() == "offline":
+    print("I am waiting for you :)")
+else:
+    print("Your input is wrong!!")
+    exit()
+
 print("\nList order")
 print(burger, "Burger =", (burger * 1000))
 print(pizza, "Pizza =", (pizza * 2000))
-print(hotdog, "Hotdog =", (hotdog * 3000))        
+print(hotdog, "Hotdog =", (hotdog * 3000))
+print("Order method =", order_method)
+if order_method == "online":
+    print("Expedition = ", expedition)
 print("\nTotal = ", total)
 
 money = int(input("Enter your money : "))
